235fa790e07149b5269de2b69fe227e0cf25e793
gorsky <takashivip@gmail.com>
2018-05-13 10:24:12 +0200

    Add directory structure and essential files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cakery',
+    version='0.1a1',
+    packages=['cakery'],
+    url='https://github.com/jelcynek/cakery',
+    license='MIT',
+    author='Gorsky',
+    author_email='takashivip@gmail.com',
+    description='Cakery is simple python framework for game development using pygame library.  '
+)
